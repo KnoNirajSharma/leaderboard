f04de2f8a5d4a44cdfc1de60f410b1972d47db85
mansi7babbar <mansi7babbar@gmail.com>
2020-04-14 23:17:22 +0530

    added .gitignore

--- app/dml/KLB-DML.sql
+++
@@ -1,6 +0,0 @@
-CREATE DATABASE knoldus_leaderboard;
-\c knoldus_leaderboard
-CREATE TABLE knolder(id SERIAL PRIMARY KEY NOT NULL, full_name VARCHAR(100) NOT NULL, wordpress_id VARCHAR(100) UNIQUE);
-CREATE TABLE blog(id INT PRIMARY KEY NOT NULL, wordpress_id VARCHAR(100) NOT NULL, published_on TIMESTAMP NOT NULL, title VARCHAR(255) NOT NULL);
-CREATE TABLE all_time(id SERIAL PRIMARY KEY NOT NULL, knolder_id INT REFERENCES knolder(id) NOT NULL, overall_score INT NOT NULL, overall_rank INT NOT NULL);
-
