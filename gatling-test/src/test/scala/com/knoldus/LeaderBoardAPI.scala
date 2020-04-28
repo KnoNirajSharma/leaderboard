2344d896836f6afef03664eb10907a5b34fef6b3
Your Name <you@example.com>
2020-04-28 17:49:34 +0530

    added the gatling test for LeaderBoard API

---
+++ gatling-test/src/test/scala/com/knoldus/LeaderBoardAPI.scala
@@ -0,0 +1,26 @@
+package com.knoldus
+
+import com.typesafe.config.ConfigFactory
+import io.gatling.core.Predef._
+import io.gatling.core.structure.ScenarioBuilder
+import io.gatling.http.Predef._
+import io.gatling.http.protocol.HttpProtocolBuilder
+
+class LeaderBoardAPI extends Simulation {
+
+  val config = ConfigFactory.load("application.conf")
+
+  val url = config.getString("url")
+
+
+  val httpProtocol: HttpProtocolBuilder = http.baseUrl(url)
+  val scene: ScenarioBuilder = scenario("KLB api test")
+    .exec(http("request1")
+      .get(url).check(status.is(200))
+      .check(jsonPath("$..rank").findAll))
+    .pause(1)
+
+  setUp(scene.inject(atOnceUsers(500)).protocols(httpProtocol))
+
+}
+
