119b674cba15d9cd253da849baabfe8f09c5cffe
abhinavsharma402 <abhinavsharma402@gmail.com>
2020-05-08 17:18:06 +0530

    ' update test cases

--- app/src/test/scala/com/knoldus/leader_board/business/NumberOfBlogsPerKnolderImplSpec.scala
+++ app/src/test/scala/com/knoldus/leader_board/business/NumberOfBlogsPerKnolderImplSpec.scala
@@ -1,71 +1,28 @@
-//package com.knoldus.leader_board.business
-//
-//import com.knoldus.leader_board._
-//import com.knoldus.leader_board.infrastructure._
-//import org.mockito.MockitoSugar
-//import org.scalatest.flatspec.AnyFlatSpec
-//
-//class NumberOfBlogsPerKnolderImplSpec extends AnyFlatSpec with MockitoSugar {
-//  val mockReadKnolder: ReadKnolder = mock[ReadKnolderImpl]
-//  val mockReadBlog: ReadBlog = mock[ReadBlogImpl]
-//  val mockReadAllTime: ReadAllTime = mock[ReadAllTimeImpl]
-//  val mockWriteAllTime: WriteAllTime = mock[WriteAllTimeImpl]
-//  val numberOfBlogs: NumberOfBlogsPerKnolder =
-//    new NumberOfBlogsPerKnolderImpl(mockReadKnolder, mockReadBlog, mockReadAllTime, mockWriteAllTime)
-//
-//  "get number of blogs " should "return number of blogs of each knolder" in {
-//    when(mockReadKnolder.fetchKnolders)
-//      .thenReturn(List(Knolder(1, "Mukesh Gupta", "mukesh01", "mukesh.kumar@knoldus.com"),
-//        Knolder(2, "Abhishek Baranwal", "abhishek02", "abhishek.baranwal@knoldus.com"),
-//        Knolder(3, "Komal Rajpal", "komal03", "komal.rajpal@knoldus.com")))
-//
-//    when(mockReadBlog.fetchKnoldersWithBlogs)
-//      .thenReturn(List(KnolderBlog(1001, "mukesh01"), KnolderBlog(1002, "abhishek02"),
-//        KnolderBlog(1003, "komal03"), KnolderBlog(1004, "mukesh01")))
-//
-//    assert(numberOfBlogs.getNumberOfBlogsPerKnolder == List(BlogCount(1, "mukesh01", "Mukesh Gupta", 2),
-//      BlogCount(2, "abhishek02", "Abhishek Baranwal", 1), BlogCount(3, "komal03", "Komal Rajpal", 1)))
-//  }
-//
-//  "get knolder blog count" should "return blog count of each knolder along with their knolder id" in {
-//    val blogCount = List(BlogCount(1, "mukesh01", "Mukesh Gupta", 2))
-//    val knolderBlogCount = List(KnolderBlogCount(Some(1), BlogCount(1, "mukesh01", "Mukesh Gupta", 2)))
-//
-//    when(mockReadAllTime.fetchKnolderIdFromAllTime(1))
-//      .thenReturn(Option(1))
-//
-//    assert(numberOfBlogs.getKnolderBlogCount(blogCount) == knolderBlogCount)
-//  }
-//
-//  "insert all time data" should "insert number of blogs in all_time table" in {
-//    val blogCount = List(KnolderBlogCount(None, BlogCount(1, "mukesh01", "Mukesh Gupta", 2)))
-//
-//    when(mockWriteAllTime.insertAllTimeData(BlogCount(1, "mukesh01", "Mukesh Gupta", 2)))
-//      .thenReturn(1)
-//
-//    assert(numberOfBlogs.insertBlogCount(blogCount).sum == 1)
-//  }
-//
-//  "insert all time data" should "not insert number of blogs in all_time table when knolder already exist in all time " +
-//    "table" in {
-//    val blogCount = List(KnolderBlogCount(Some(1), BlogCount(1, "mukesh01", "Mukesh Gupta", 2)))
-//
-//    assert(numberOfBlogs.insertBlogCount(blogCount).sum == 0)
-//  }
-//
-//  "update all time " should "update number of blogs in all_time table" in {
-//    val blogCount = List(KnolderBlogCount(Option(1), BlogCount(1, "mukesh01", "Mukesh Gupta", 2)))
-//
-//    when(mockWriteAllTime.updateAllTimeData(BlogCount(1, "mukesh01", "Mukesh Gupta", 2)))
-//      .thenReturn(1)
-//
-//    assert(numberOfBlogs.updateBlogCount(blogCount).sum == 1)
-//  }
-//
-//  "update all time " should "not update number of blogs in all_time table when knolder does not exist in all time " +
-//    "table" in {
-//    val blogCount = List(KnolderBlogCount(None, BlogCount(1, "mukesh01", "Mukesh Gupta", 2)))
-//
-//    assert(numberOfBlogs.updateBlogCount(blogCount).sum == 0)
-//  }
-//}
+package com.knoldus.leader_board.business
+
+import com.knoldus.leader_board._
+import com.knoldus.leader_board.infrastructure._
+import org.mockito.MockitoSugar
+import org.scalatest.flatspec.AnyFlatSpec
+
+class NumberOfBlogsPerKnolderImplSpec extends AnyFlatSpec with MockitoSugar {
+  val mockReadKnolder: ReadKnolder = mock[ReadKnolderImpl]
+  val mockReadBlog: ReadBlog = mock[ReadBlogImpl]
+  val mockReadAllTime: ReadAllTime = mock[ReadAllTimeImpl]
+  val mockWriteAllTime: WriteAllTime = mock[WriteAllTimeImpl]
+  val numberOfBlogs: NumberOfBlogsPerKnolder =
+    new NumberOfBlogsPerKnolderImpl(mockReadKnolder, mockReadBlog, mockReadAllTime, mockWriteAllTime)
+
+  "get knolder blog count" should "return blog count of each knolder along with their knolder id" in {
+
+    when(mockReadBlog.fetchKnoldersWithBlogs)
+      .thenReturn(List(BlogCount(1, "mukesh01", "Mukesh Gupta", 2)))
+    val blogCount = List(BlogCount(1, "mukesh01", "Mukesh Gupta", 2))
+    val knolderBlogCount = List(KnolderBlogCount(Some(1), BlogCount(1, "mukesh01", "Mukesh Gupta", 2)))
+
+    when(mockReadAllTime.fetchKnolderIdFromAllTime(1))
+      .thenReturn(Option(1))
+
+    assert(numberOfBlogs.getKnolderBlogCount == knolderBlogCount)
+  }
+}
--- app/src/test/scala/com/knoldus/leader_board/business/OverallReputationImplSpec.scala
+++ app/src/test/scala/com/knoldus/leader_board/business/OverallReputationImplSpec.scala
@@ -1,67 +1,67 @@
-//package com.knoldus.leader_board.business
-//
-//import com.knoldus.leader_board.infrastructure.{ReadAllTime, ReadAllTimeImpl}
-//import com.knoldus.leader_board.{GetAllTime, Reputation}
-//import com.typesafe.config.ConfigFactory
-//import org.mockito.MockitoSugar
-//import org.scalatest.flatspec.AnyFlatSpec
-//
-//class OverallReputationImplSpec extends AnyFlatSpec with MockitoSugar {
-//  val mockReadAllTime: ReadAllTime = mock[ReadAllTimeImpl]
-//  val overallReputation: OverallReputation = new OverallReputationImpl(mockReadAllTime, ConfigFactory.load())
-//
-//  "calculate reputation" should "give overall reputation of each knolder" in {
-//    val allTimeData = List(GetAllTime("Mukesh Gupta", Option(2)),
-//      GetAllTime("Komal Rajpal", Option(1)),
-//      GetAllTime("Abhishek Baranwal", Option(1)))
-//    when(mockReadAllTime.fetchAllTimeData)
-//      .thenReturn(allTimeData)
-//    assert(overallReputation.calculateReputation ==
-//      List(Reputation("Mukesh Gupta", 10, 1), Reputation("Abhishek Baranwal", 5, 2), Reputation("Komal Rajpal", 5, 2)))
-//  }
-//
-//  "calculate reputation" should "give overall reputation of each knolder if one knolder has 0 blog count" in {
-//    val allTimeData = List(GetAllTime("Mukesh Gupta", Option(2)),
-//      GetAllTime("Komal Rajpal", Option(1)),
-//      GetAllTime("Abhishek Baranwal", None))
-//    when(mockReadAllTime.fetchAllTimeData)
-//      .thenReturn(allTimeData)
-//    assert(overallReputation.calculateReputation ==
-//      List(Reputation("Mukesh Gupta", 10, 1), Reputation("Komal Rajpal", 5, 2), Reputation("Abhishek Baranwal", 0, 3)))
-//  }
-//
-//  "calculate reputation" should "give overall reputation of each knolder if list of all time data has no element" in {
-//    val allTimeData = List()
-//    when(mockReadAllTime.fetchAllTimeData)
-//      .thenReturn(allTimeData)
-//    assert(overallReputation.calculateReputation == List(Reputation("", 0, 1)))
-//  }
-//
-//  "calculate reputation" should "give overall reputation of each knolder if list of all time data has one element" in {
-//    val allTimeData = List(GetAllTime("Mukesh Gupta", Option(2)))
-//    when(mockReadAllTime.fetchAllTimeData)
-//      .thenReturn(allTimeData)
-//    assert(overallReputation.calculateReputation == List(Reputation("Mukesh Gupta", 10, 1)))
-//  }
-//
-//  "calculate reputation" should "give overall reputation of each knolder if list of all time data has two element" +
-//    "and first and second blog counts are not equal" in {
-//    val allTimeData = List(GetAllTime("Mukesh Gupta", Option(2)),
-//      GetAllTime("Komal Rajpal", Option(1)))
-//    when(mockReadAllTime.fetchAllTimeData)
-//      .thenReturn(allTimeData)
-//    assert(overallReputation.calculateReputation == List(Reputation("Mukesh Gupta", 10, 1),
-//      Reputation("Komal Rajpal", 5, 2)))
-//  }
-//
-//  "calculate reputation" should "give overall reputation of each knolder if list of all time data has two element" +
-//    "and first and second blog counts are equal" in {
-//    val allTimeData = List(GetAllTime("Mukesh Gupta", Option(2)),
-//      GetAllTime("Komal Rajpal", Option(2)),
-//      GetAllTime("Abhishek Baranwal", Option(1)))
-//    when(mockReadAllTime.fetchAllTimeData)
-//      .thenReturn(allTimeData)
-//    assert(overallReputation.calculateReputation ==
-//      List(Reputation("Komal Rajpal", 10, 1), Reputation("Mukesh Gupta", 10, 1), Reputation("Abhishek Baranwal", 5, 2)))
-//  }
-//}
+package com.knoldus.leader_board.business
+
+import com.knoldus.leader_board.infrastructure.{ReadAllTime, ReadAllTimeImpl}
+import com.knoldus.leader_board.{GetAllTime, Reputation}
+import com.typesafe.config.ConfigFactory
+import org.mockito.MockitoSugar
+import org.scalatest.flatspec.AnyFlatSpec
+
+class OverallReputationImplSpec extends AnyFlatSpec with MockitoSugar {
+  val mockReadAllTime: ReadAllTime = mock[ReadAllTimeImpl]
+  val overallReputation: OverallReputation = new OverallReputationImpl(mockReadAllTime, ConfigFactory.load())
+
+  "calculate reputation" should "give overall reputation of each knolder" in {
+    val allTimeData = List(GetAllTime(1,"Mukesh Gupta", Option(2)),
+      GetAllTime(2,"Komal Rajpal", Option(1)),
+      GetAllTime(3,"Abhishek Baranwal", Option(1)))
+    when(mockReadAllTime.fetchAllTimeData)
+      .thenReturn(allTimeData)
+    assert(overallReputation.calculateReputation ==
+      List(Reputation(1,"Mukesh Gupta", 10, 1), Reputation(3,"Abhishek Baranwal", 5, 2), Reputation(2,"Komal Rajpal", 5, 2)))
+  }
+
+  "calculate reputation" should "give overall reputation of each knolder if one knolder has 0 blog count" in {
+    val allTimeData = List(GetAllTime(1,"Mukesh Gupta", Option(2)),
+      GetAllTime(2,"Komal Rajpal", Option(1)),
+      GetAllTime(3,"Abhishek Baranwal", None))
+    when(mockReadAllTime.fetchAllTimeData)
+      .thenReturn(allTimeData)
+    assert(overallReputation.calculateReputation ==
+      List(Reputation(1,"Mukesh Gupta", 10, 1), Reputation(2,"Komal Rajpal", 5, 2), Reputation(3,"Abhishek Baranwal", 0, 3)))
+  }
+
+  "calculate reputation" should "give overall reputation of each knolder if list of all time data has no element" in {
+    val allTimeData = List()
+    when(mockReadAllTime.fetchAllTimeData)
+      .thenReturn(allTimeData)
+    assert(overallReputation.calculateReputation == List(Reputation(0,"", 0, 1)))
+  }
+
+  "calculate reputation" should "give overall reputation of each knolder if list of all time data has one element" in {
+    val allTimeData = List(GetAllTime(1,"Mukesh Gupta", Option(2)))
+    when(mockReadAllTime.fetchAllTimeData)
+      .thenReturn(allTimeData)
+    assert(overallReputation.calculateReputation == List(Reputation(1,"Mukesh Gupta", 10, 1)))
+  }
+
+  "calculate reputation" should "give overall reputation of each knolder if list of all time data has two element" +
+    "and first and second blog counts are not equal" in {
+    val allTimeData = List(GetAllTime(1,"Mukesh Gupta", Option(2)),
+      GetAllTime(2,"Komal Rajpal", Option(1)))
+    when(mockReadAllTime.fetchAllTimeData)
+      .thenReturn(allTimeData)
+    assert(overallReputation.calculateReputation == List(Reputation(1,"Mukesh Gupta", 10, 1),
+      Reputation(2,"Komal Rajpal", 5, 2)))
+  }
+
+  "calculate reputation" should "give overall reputation of each knolder if list of all time data has two element" +
+    "and first and second blog counts are equal" in {
+    val allTimeData = List(GetAllTime(1,"Mukesh Gupta", Option(2)),
+      GetAllTime(2,"Komal Rajpal", Option(2)),
+      GetAllTime(3,"Abhishek Baranwal", Option(1)))
+    when(mockReadAllTime.fetchAllTimeData)
+      .thenReturn(allTimeData)
+    assert(overallReputation.calculateReputation ==
+      List(Reputation(2,"Komal Rajpal", 10, 1), Reputation(1,"Mukesh Gupta", 10, 1), Reputation(3,"Abhishek Baranwal", 5, 2)))
+  }
+}
