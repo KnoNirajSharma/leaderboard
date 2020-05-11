c8d19b5ba66f420d6f609b8222ddd1fabf928fe8
mansi7babbar <mansi7babbar@gmail.com>
2020-05-11 12:38:47 +0530

    updated test cases for infrastructure

--- app/src/test/scala/com/knoldus/leader_board/infrastructure/MainSpec.scala
+++ app/src/test/scala/com/knoldus/leader_board/infrastructure/MainSpec.scala
@@ -2,5 +2,5 @@ package com.knoldus.leader_board.infrastructure
 
 import org.scalatest.Suites
 
-class MainSpec extends Suites(new ReadKnolderImplSpec, new ReadBlogImplSpec,
-  new WriteAllTimeImplSpec, new ReadAllTimeImplSpec)
+class MainSpec extends Suites(new ReadBlogImplSpec, new WriteAllTimeImplSpec, new ReadAllTimeImplSpec,
+  new WriteAllTimeReputationImplSpec, new ReadAllTimeReputationImplSpec)
