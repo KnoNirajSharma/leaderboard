e64ea4db3ddb3b1e9f3147b8614fd23ab602cef0
Your Name <you@example.com>
2020-05-13 19:00:12 +0530

    removed the irrelevant spaces

--- gatling_performance_test/src/test/scala/com/knoldus/GatlingTest.scala
+++ gatling_performance_test/src/test/scala/com/knoldus/GatlingTest.scala
@@ -1,12 +1,10 @@
 package com.knoldus
-
 import com.typesafe.config.{Config, ConfigFactory}
 import io.gatling.core.Predef._
 import io.gatling.core.scenario.Simulation
 import io.gatling.core.structure.ScenarioBuilder
 import io.gatling.http.Predef._
 import io.gatling.http.protocol.HttpProtocolBuilder
-
 class GatlingTest extends Simulation {
   val config: Config = ConfigFactory.load("application.conf")
   val url: String = config.getString("url")
@@ -17,9 +15,7 @@ class GatlingTest extends Simulation {
   val ConstantUsersFrequency: Int = config.getInt("constantUsersFrequency")
   val RampUsersPerStartingRate: Int = config.getInt("rampUsersPerSecond")
   val RampUsersPerTargetRate: Int = config.getInt("rampUsersPerSecondTarget")
-
   val OK = 200
-
   def createScenarioBuilder(scenarioName: String, requestName: String, path: String, pauseDuration: Int): ScenarioBuilder = {
     /**
      * generic method for creating a scenario
@@ -40,7 +36,6 @@ class GatlingTest extends Simulation {
     val constantUserPerSecScenario = createScenarioBuilder("constant users per second", "request1", "$..rank", 0)
     val rampUserPerSecScenario = createScenarioBuilder("ramp users per second", "request1", "$..rank", 0)
     val httpProtocol: HttpProtocolBuilder = http.baseUrl(url)
-
     setUp(atOnceScenario.inject(atOnceUsers(AtOnceUsers)).protocols(httpProtocol),
       rampUpUsersScenario.inject(rampUsers(RampUsers) during RampFrequency).protocols(httpProtocol),
       constantUserPerSecScenario.inject(constantUsersPerSec(ConstantUsers) during ConstantUsersFrequency).protocols(httpProtocol),
@@ -48,12 +43,3 @@ class GatlingTest extends Simulation {
   }
   setUp()
 }
-
-/*
-setUp(scenarioBuilder.inject(atOnceUsers(users),
-      rampUsers(10) during (5 ),
-      constantUsersPerSec(20) during (15 ),
-      rampUsersPerSec(10) to 20 during (10 )).protocols(httpProtocol))
-//  setUp(scenarioBuilder.inject(atOnceUsers(10),rampUsers(users) during 5).protocols(httpProtocol))
-
- */
