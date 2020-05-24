a7d071060c091fcea9302c3ea082927367dba266
abhinavsharma402 <abhinavsharma402@gmail.com>
2020-05-24 17:19:24 +0530

    update resources folder

--- app/src/main/resources/createTable.sql
+++ app/src/main/resources/createTable.sql
@@ -12,12 +12,6 @@ published_on TIMESTAMP NOT NULL,
 title VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS all_time(
-id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-knolder_id INT NOT NULL,
-number_of_blogs INT
-);
-
 CREATE TABLE IF NOT EXISTS all_time_reputation(
 id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
 knolder_id INT NOT NULL,
@@ -32,4 +26,11 @@ knolder_id INT NOT NULL,
 full_name VARCHAR(100) NOT NULL,
 score INT NOT NULL,
 rank INT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS quarterly_reputation(
+id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+knolder_id INT NOT NULL,
+full_name VARCHAR(100) NOT NULL,
+streak VARCHAR(20) NOT NULL
 );
