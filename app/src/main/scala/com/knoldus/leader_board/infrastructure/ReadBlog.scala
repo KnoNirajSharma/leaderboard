6035d3b682a3b824daf4b76dfe2666e5110aba4c
abhinavsharma402 <abhinavsharma402@gmail.com>
2020-05-24 17:20:07 +0530

    update source code

--- app/src/main/scala/com/knoldus/leader_board/infrastructure/ReadBlog.scala
+++ app/src/main/scala/com/knoldus/leader_board/infrastructure/ReadBlog.scala
@@ -1,9 +1,15 @@
 package com.knoldus.leader_board.infrastructure
 
-import com.knoldus.leader_board.{BlogCount, GetMonthlyCount}
+import com.knoldus.leader_board.GetCount
 
 trait ReadBlog {
-  def fetchKnoldersWithBlogs: List[BlogCount]
+  def fetchKnoldersWithBlogs: List[GetCount]
 
-  def fetchKnoldersWithMonthlyBlogs: List[GetMonthlyCount]
+  def fetchKnoldersWithMonthlyBlogs: List[GetCount]
+
+  def fetchKnoldersWithQuarterFirstMonthBlogs: List[GetCount]
+
+  def fetchKnoldersWithQuarterSecondMonthBlogs: List[GetCount]
+
+  def fetchKnoldersWithQuarterThirdMonthBlogs: List[GetCount]
 }
