a8a5a505f191efe8b970e7e04593a3a2558c82b1
mansi7babbar <mansi7babbar@gmail.com>
2020-05-27 20:03:48 +0530

    fetched details of particular knolder by passing knolder id

--- app/src/main/scala/com/knoldus/leader_board/Model.scala
+++ app/src/main/scala/com/knoldus/leader_board/Model.scala
@@ -1,5 +1,7 @@
 package com.knoldus.leader_board
 
+import java.sql.Timestamp
+
 final case class GetCount(knolderId: Int, knolderName: String, numberOfBlogs: Int)
 
 final case class GetScore(knolderId: Int, knolderName: String, score: Int)
@@ -12,5 +14,12 @@ final case class GetStreak(knolderId: Int, knolderName: String, streak: String)
 
 final case class KnolderStreak(knolderId: Option[Int], streak: GetStreak)
 
-final case class Reputation(knolderName: String, allTimeScore: Int, allTimeRank: Int, quarterlyStreak: String,
-                            monthlyScore: Int, monthlyRank: Int)
+final case class Reputation(knolderId: Int, knolderName: String, allTimeScore: Int, allTimeRank: Int,
+                            quarterlyStreak: String, monthlyScore: Int, monthlyRank: Int)
+
+final case class ContributionScores(knolderName: String, allTimeScore: Int, monthlyScore: Int, blogScore: Int)
+
+final case class ContributionTitles(title: String, date: Timestamp)
+
+final case class KnolderDetails(knolderName: String, allTimeScore: Int, monthlyScore: Int, blogScore: Int,
+                                blogDetails: List[ContributionTitles])
---
+++ app/src/main/scala/com/knoldus/leader_board/infrastructure/FetchKnolderDetailsImpl.scala
@@ -0,0 +1,47 @@
+package com.knoldus.leader_board.infrastructure
+
+import java.sql.{Connection, Timestamp}
+
+import com.knoldus.leader_board._
+import com.typesafe.config.Config
+import com.typesafe.scalalogging.LazyLogging
+import scalikejdbc.{DB, DBSession, SQL}
+
+class FetchKnolderDetailsImpl(config: Config) extends FetchKnolderDetails with LazyLogging {
+  implicit val connection: Connection = DatabaseConnection.connection(config)
+  implicit val session: DBSession = DB.readOnlySession()
+
+  /**
+   * Fetching details of each knolder.
+   *
+   * @return List of details of knolders.
+   */
+  override def fetchKnolderDetails(knolderId: Int): Option[KnolderDetails] = {
+    logger.info("Fetching details of each knolder.")
+
+    val currentMonth = Timestamp.valueOf(Constant.CURRENT_TIME
+      .withDayOfMonth(1).toLocalDate.atStartOfDay())
+
+    val nextMonth = Timestamp.valueOf(Constant.CURRENT_TIME
+      .withDayOfMonth(1).toLocalDate.plusMonths(1).atStartOfDay())
+
+    val scores = SQL("SELECT knolder.full_name, all_time_reputation.score AS allTimeScore, monthly_reputation.score " +
+      s"AS monthlyScore,COUNT(blog.title) * ${config.getInt("scorePerBlog")} As blogScore FROM knolder INNER JOIN " +
+      s"all_time_reputation ON knolder.id = all_time_reputation.knolder_id " + "INNER JOIN monthly_reputation ON " +
+      "knolder.id = monthly_reputation.knolder_id LEFT JOIN blog ON knolder.wordpress_id = blog.wordpress_id AND " +
+      "published_on >= ? AND published_on < ? WHERE knolder.id = ? GROUP BY knolder.full_name, all_time_reputation.score, " +
+      "monthly_reputation.score")
+      .bind(currentMonth, nextMonth, knolderId)
+      .map(rs => ContributionScores(rs.string("full_name"), rs.int("allTimeScore"),
+        rs.int("monthlyScore"), rs.int("blogScore"))).single().apply()
+
+    val titles = SQL("SELECT blog.title, blog.published_on FROM knolder LEFT JOIN blog ON knolder.wordpress_id = " +
+      "blog.wordpress_id WHERE knolder.id = ? AND published_on >= ? AND published_on < ?")
+      .bind(knolderId, currentMonth, nextMonth)
+      .map(rs => ContributionTitles(rs.string("title"), rs.timestamp("published_on")))
+      .list().apply()
+
+    scores.map(scores => KnolderDetails(scores.knolderName, scores.allTimeScore, scores.monthlyScore, scores.blogScore,
+      titles))
+  }
+}
