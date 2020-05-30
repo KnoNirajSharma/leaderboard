ff754beb3fcb72237a20ab7ff66cbdb1dc9dd0ad
umang-goyal <goel97umang@gmail.com>
2020-05-30 23:39:08 +0530

    new table setup according

--- ui/src/app/models/author.model.ts
+++ ui/src/app/models/author.model.ts
@@ -1,8 +1,9 @@
 export interface AuthorModel {
+    knolderId: number;
     knolderName: string;
-    score?: number;
-    rank?: number;
-    quarterlyStreak?: string;
-    monthlyScore?: number;
-    monthlyRank?: number;
+    allTimeScore: number;
+    allTimeRank: number;
+    quarterlyStreak: string;
+    monthlyScore: number;
+    monthlyRank: number;
 }
