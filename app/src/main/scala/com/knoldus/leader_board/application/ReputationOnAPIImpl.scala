0567311cd699281b5ef9bd292caa773f64cc7a11
abhinavsharma402 <abhinavsharma402@gmail.com>
2020-06-06 11:41:07 +0530

    update code with test cases

--- app/src/main/scala/com/knoldus/leader_board/application/ReputationOnAPIImpl.scala
+++ app/src/main/scala/com/knoldus/leader_board/application/ReputationOnAPIImpl.scala
@@ -1,11 +1,9 @@
 package com.knoldus.leader_board.application
-
 import java.time.Month
-
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
-import akka.http.scaladsl.server.Directives
+import akka.http.scaladsl.server.{Directives, Route}
 import ch.megard.akka.http.cors.scaladsl.CorsDirectives
 import ch.megard.akka.http.cors.scaladsl.settings.CorsSettings
 import com.knoldus.leader_board.infrastructure.{FetchKnolderDetails, FetchReputation}
@@ -13,22 +11,31 @@ import com.typesafe.config.Config
 import com.typesafe.scalalogging._
 import net.liftweb.json.Extraction.decompose
 import net.liftweb.json.{DefaultFormats, compactRender}
-
 import scala.concurrent.{ExecutionContextExecutor, Future}
-
 class ReputationOnAPIImpl(fetchKnolderDetails: FetchKnolderDetails, fetchReputation: FetchReputation, config: Config)
                          (implicit system: ActorSystem, executionContext: ExecutionContextExecutor)
   extends ReputationOnAPI with Directives with CorsDirectives with LazyLogging {
   implicit val formats: DefaultFormats.type = net.liftweb.json.DefaultFormats
-
   /**
-   * Displays reputation of each knolder on API.
+   * Displays reputation and details of each knolder on API.
    *
    * @return Http request binded with server port.
    */
   override def displayReputationOnAPI: Future[Http.ServerBinding] = {
-    logger.info("Displaying reputation of each knolder on the API.")
-    val reputationRoute = cors(settings = CorsSettings.defaultSettings) {
+    val mainRoute = reputationRoute ~ monthlyDetailsRoute ~ allTimeDetailsRoute
+    Http().bindAndHandle(mainRoute, config.getString("interface"), config.getInt("port"))
+      .recoverWith {
+        case ex: Exception => Future.failed(new Exception("Service failed", ex))
+      }
+  }
+  /**
+   * Displays reputation of each knolder on API.
+   *
+   * @return Route for displaying reputation of each knolder on API.
+   */
+  override def reputationRoute: Route = {
+    logger.info("Displaying reputation of each knolder on API.")
+    cors(settings = CorsSettings.defaultSettings) {
       path("reputation") {
         get {
           complete(HttpEntity(ContentTypes.`application/json`,
@@ -36,8 +43,15 @@ class ReputationOnAPIImpl(fetchKnolderDetails: FetchKnolderDetails, fetchReputat
         }
       }
     }
-    logger.info("Displaying monthly details of each knolder on the API.")
-    val monthlyDetailsRoute = cors(settings = CorsSettings.defaultSettings) {
+  }
+  /**
+   * Displays monthly details of particular knolder on API.
+   *
+   * @return Route for displaying monthly details of particular knolder on API.
+   */
+  override def monthlyDetailsRoute: Route = {
+    logger.info("Displaying monthly details of particular knolder on API.")
+    cors(settings = CorsSettings.defaultSettings) {
       path("reputation" / IntNumber) { id =>
         parameters("month", "year") { (month, year) =>
           get {
@@ -48,8 +62,15 @@ class ReputationOnAPIImpl(fetchKnolderDetails: FetchKnolderDetails, fetchReputat
         }
       }
     }
-    logger.info("Displaying all time details of each knolder on the API.")
-    val allTimeDetailsRoute = cors(settings = CorsSettings.defaultSettings) {
+  }
+  /**
+   * Displays all time details of particular knolder on API.
+   *
+   * @return Route for displaying all time details of particular knolder on API.
+   */
+  override def allTimeDetailsRoute: Route = {
+    logger.info("Displaying all time details of particular knolder on API.")
+    cors(settings = CorsSettings.defaultSettings) {
       path("reputation" / IntNumber) { id =>
         get {
           complete(HttpEntity(ContentTypes.`application/json`,
@@ -57,10 +78,5 @@ class ReputationOnAPIImpl(fetchKnolderDetails: FetchKnolderDetails, fetchReputat
         }
       }
     }
-    val mainRoute = reputationRoute ~ monthlyDetailsRoute ~ allTimeDetailsRoute
-    Http().bindAndHandle(mainRoute, config.getString("interface"), config.getInt("port"))
-  }.recoverWith {
-    case ex: Exception => Future.failed(new Exception("Service failed", ex))
   }
 }
-
--- app/src/test/scala/com/knoldus/leader_board/application/ReputationOnAPIImplSpec.scala
+++ app/src/test/scala/com/knoldus/leader_board/application/ReputationOnAPIImplSpec.scala
@@ -1,44 +1,44 @@
 package com.knoldus.leader_board.application
-
-import akka.http.scaladsl.model._
-import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.testkit.ScalatestRouteTest
-import com.knoldus.leader_board.Reputation
 import com.knoldus.leader_board.infrastructure.{FetchKnolderDetails, FetchReputation}
+import com.knoldus.leader_board.{Contribution, ContributionDetails, KnolderDetails, Reputation}
 import com.typesafe.config.ConfigFactory
 import net.liftweb.json.Extraction.decompose
 import net.liftweb.json.{DefaultFormats, compactRender}
 import org.mockito.MockitoSugar
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpecLike
-
 class ReputationOnAPIImplSpec extends AnyWordSpecLike with MockitoSugar with Matchers with ScalatestRouteTest {
   implicit val formats: DefaultFormats.type = net.liftweb.json.DefaultFormats
   val mockFetchReputation: FetchReputation = mock[FetchReputation]
   val mockFetchKnolderDetails: FetchKnolderDetails = mock[FetchKnolderDetails]
   val reputationOnAPI: ReputationOnAPI = new ReputationOnAPIImpl(mockFetchKnolderDetails, mockFetchReputation,
     ConfigFactory.load())
-
   val reputations = List(Reputation(1, "Mukesh Gupta", 10, 1, "15-20-20", 10, 1),
     Reputation(2, "Abhishek Baranwal", 5, 2, "10-10-15", 5, 2),
     Reputation(3, "Komal Rajpal", 5, 2, "5-10-5", 5, 2))
-
+  val blogTitles = List(ContributionDetails("windows handling using selenium webdriver", "2020-04-13 13:10:40"),
+    ContributionDetails("Java 9: Enhance your Jav…ptional API enhancement", "2020-04-13 13:10:40"))
+  val blogDetails: Option[Contribution] = Option(Contribution("Blog", 2, 10, blogTitles))
+  val contributions = List(blogDetails)
+  val knolderDetails: Option[KnolderDetails] = Option(KnolderDetails("Mukesh Gupta", 10, contributions))
   when(mockFetchReputation.fetchReputation).thenReturn(reputations)
-
+  when(mockFetchKnolderDetails.fetchKnolderMonthlyDetails(1, 4, 2020)).thenReturn(knolderDetails)
+  when(mockFetchKnolderDetails.fetchKnolderAllTimeDetails(1)).thenReturn(knolderDetails)
   "The service" should {
-
     "display reputation of knolders to routed path" in {
-      val route = {
-        path("reputation") {
-          get {
-            complete(HttpEntity(ContentTypes.`application/json`, compactRender(decompose(reputations))))
-          }
-        }
+      Get("/reputation") ~> reputationOnAPI.reputationRoute ~> check {
+        responseAs[String] shouldEqual compactRender(decompose(reputations))
       }
-      Get("/reputation") ~> route ~> check {
-        reputationOnAPI.displayReputationOnAPI.map { displayedReputation =>
-          responseAs[String] shouldEqual displayedReputation
-        }
+    }
+    "display monthly details of knolders to routed path" in {
+      Get("/reputation/1?month=April&year=2020") ~> reputationOnAPI.monthlyDetailsRoute ~> check {
+        responseAs[String] shouldEqual compactRender(decompose(knolderDetails))
+      }
+    }
+    "display all time details of knolders to routed path" in {
+      Get("/reputation/1") ~> reputationOnAPI.allTimeDetailsRoute ~> check {
+        responseAs[String] shouldEqual compactRender(decompose(knolderDetails))
       }
     }
   }
