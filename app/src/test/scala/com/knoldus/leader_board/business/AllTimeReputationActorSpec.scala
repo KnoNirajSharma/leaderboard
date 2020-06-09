381dfafa459c71b453e9318a741c923622fe3d20
mansi7babbar <mansi7babbar@gmail.com>
2020-06-09 17:58:47 +0530

    add unit test cases for actors of monthly reputation and all time reputation

---
+++ app/src/test/scala/com/knoldus/leader_board/business/AllTimeReputationActorSpec.scala
@@ -0,0 +1,60 @@
+package com.knoldus.leader_board.business
+
+import java.sql.Connection
+
+import akka.actor.{ActorSystem, Props}
+import akka.testkit.{ImplicitSender, TestKit, TestProbe}
+import com.knoldus.leader_board.infrastructure.{WriteAllTimeReputation, WriteAllTimeReputationImpl}
+import com.knoldus.leader_board.{DatabaseConnection, GetReputation, KnolderReputation}
+import com.typesafe.config.ConfigFactory
+import org.mockito.MockitoSugar
+import org.scalatest.BeforeAndAfterAll
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpecLike
+
+class AllTimeReputationActorSpec extends TestKit(ActorSystem("AllTimeActorSpec")) with ImplicitSender
+  with AnyWordSpecLike with Matchers with BeforeAndAfterAll with MockitoSugar {
+  implicit val connection: Connection = DatabaseConnection.connection(ConfigFactory.load())
+  val mockAllTimeReputation: AllTimeReputation = mock[AllTimeReputationImpl]
+  val mockWriteAllTimeReputation: WriteAllTimeReputation = mock[WriteAllTimeReputationImpl]
+
+  override def afterAll: Unit = {
+    TestKit.shutdownActorSystem(system)
+  }
+
+  "Quarterly reputation actor" must {
+    "insert and update all time reputation" in {
+      val probe = TestProbe()
+      val reputationOfKnolders = List(KnolderReputation(None, GetReputation(1, "Mukesh Gupta", 10, 1)),
+        KnolderReputation(None, GetReputation(2, "anjali", 5, 2)))
+      when(mockAllTimeReputation.getKnolderReputation)
+        .thenReturn(reputationOfKnolders)
+      when(mockWriteAllTimeReputation.insertAllTimeReputationData(reputationOfKnolders))
+        .thenReturn(List(2))
+      when(mockWriteAllTimeReputation.updateAllTimeReputationData(reputationOfKnolders))
+        .thenReturn(List(2))
+      val quarterlyReputationActor = system.actorOf(Props(new AllTimeReputationActor(mockAllTimeReputation,
+        mockWriteAllTimeReputation)))
+      probe watch quarterlyReputationActor
+      probe.send(quarterlyReputationActor, "write all time reputation")
+      probe.expectMsg("all time reputation saved")
+    }
+
+    "insert and update all time reputation with incorrect message" in {
+      val probe = TestProbe()
+      val reputationOfKnolders = List(KnolderReputation(None, GetReputation(1, "Mukesh Gupta", 10, 1)),
+        KnolderReputation(None, GetReputation(2, "anjali", 5, 2)))
+      when(mockAllTimeReputation.getKnolderReputation)
+        .thenReturn(reputationOfKnolders)
+      when(mockWriteAllTimeReputation.insertAllTimeReputationData(reputationOfKnolders))
+        .thenReturn(List(2))
+      when(mockWriteAllTimeReputation.updateAllTimeReputationData(reputationOfKnolders))
+        .thenReturn(List(2))
+      val quarterlyReputationActor = system.actorOf(Props(new AllTimeReputationActor(mockAllTimeReputation,
+        mockWriteAllTimeReputation)))
+      probe watch quarterlyReputationActor
+      probe.send(quarterlyReputationActor, "read all time reputation")
+      probe.expectMsg("invalid message")
+    }
+  }
+}
