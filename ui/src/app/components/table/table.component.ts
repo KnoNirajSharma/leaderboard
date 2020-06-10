a331b1ee305326ec94fd0d415dc8e73bf7e13fae
Sanjoli Varshney <varshney.sanjoli@gmail.com>
2020-06-11 00:40:06 +0530

    Improved styling

--- ui/src/app/components/table/table.component.ts
+++ ui/src/app/components/table/table.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
 import {AuthorModel} from '../../models/author.model';
 import {DrilldownModalPage} from '../../pages/drilldown-modal/drilldown-modal.page';
 import {ModalController} from '@ionic/angular';
@@ -7,14 +7,18 @@ import {ModalController} from '@ionic/angular';
     selector: 'app-table',
     templateUrl: './table.component.html',
     styleUrls: ['./table.component.scss'],
+    encapsulation: ViewEncapsulation.None,
 })
 
 export class TableComponent implements OnInit {
     @Input() tableRows: AuthorModel[];
     @Input() dataKeys: string[];
-    columns = [{ name: 'Name', prop: 'knolderName'}, { name: 'Overall Score', prop: 'allTimeScore'},
-        { name: 'Overall Rank', prop: 'allTimeRank'}, { name: 'Monthly Score', prop: 'monthlyScore'},
-        { name: 'Monthly Rank', prop: 'monthlyScore'}, { name: '3 Month Streak', prop: 'quarterlyStreak', sortable: false}];
+    columns = [{ name: 'Name', prop: 'knolderName', headerClass: 'my-custom-header', cellClass: 'my-custom-cell'},
+        { name: 'Overall Score', prop: 'allTimeScore', headerClass: 'my-custom-header', cellClass: 'my-custom-cell'},
+        { name: 'Overall Rank', prop: 'allTimeRank', headerClass: 'my-custom-header', cellClass: 'my-custom-cell'},
+        { name: 'Monthly Score', prop: 'monthlyScore', headerClass: 'my-custom-header', cellClass: 'my-custom-cell'},
+        { name: 'Monthly Rank', prop: 'monthlyScore', headerClass: 'my-custom-header', cellClass: 'my-custom-cell'},
+        { name: '3 Month Streak', prop: 'quarterlyStreak', sortable: false, headerClass: 'my-custom-header', cellClass: 'my-custom-cell'}];
 
     constructor(private modalController: ModalController) {
     }
