56bb481ea69a64a724977732163035c1083db23d
mansi7babbar <mansi7babbar@gmail.com>
2020-06-23 17:40:51 +0530

    created a scheduler for fetching and storing latest knolx details

--- app/src/main/scala/com/knoldus/leader_board/DriverApp.scala
+++ app/src/main/scala/com/knoldus/leader_board/DriverApp.scala
@@ -6,6 +6,7 @@ import akka.actor.{ActorSystem, Props}
 import com.knoldus.leader_board.application.{ReputationOnAPI, ReputationOnAPIImpl}
 import com.knoldus.leader_board.business._
 import com.knoldus.leader_board.infrastructure._
+import com.typesafe.akka.extension.quartz.QuartzSchedulerExtension
 import com.typesafe.config.{Config, ConfigFactory}
 
 import scala.concurrent.ExecutionContextExecutor
@@ -15,10 +16,9 @@ object DriverApp extends App {
   implicit val system: ActorSystem = ActorSystem()
   implicit val executionContext: ExecutionContextExecutor = system.dispatcher
   val config: Config = ConfigFactory.load()
-
   val knolderScore: KnolderScore = new KnolderScoreImpl(config)
   val knolderRank: KnolderRank = new KnolderRankImpl
-  val readBlog = new ReadBlogImpl(config)
+  val readBlog = new ReadContributionImpl(config)
   val readAllTimeReputation: ReadAllTimeReputation = new ReadAllTimeReputationImpl(config)
   val writeAllTimeReputation: WriteAllTimeReputation = new WriteAllTimeReputationImpl(config)
   val allTimeReputation: AllTimeReputation = new AllTimeReputationImpl(readBlog, knolderRank, knolderScore,
@@ -36,20 +36,24 @@ object DriverApp extends App {
   val reputationOnAPI: ReputationOnAPI = new ReputationOnAPIImpl(fetchKnolderDetails, fetchReputation, config)
   val fetchBlogs: FetchBlogs = new FetchBlogsImpl(config)
   val storeBlogs: StoreBlogs = new StoreBlogsImpl(config)
+  val storeKnolx: StoreKnolx = new StoreKnolxImpl(config)
   val URLResponse: URLResponse = new URLResponse
   val blogs: Blogs = new BlogsImpl(fetchBlogs, URLResponse, config)
-
+  val knolx: Knolxs = new KnolxImpl(URLResponse, config)
   val allTimeReputationActorRef = system.actorOf(Props(new AllTimeReputationActor(allTimeReputation,
     writeAllTimeReputation)), "allTimeReputationActor")
   val monthlyReputationActorRef = system.actorOf(Props(new MonthlyReputationActor(monthlyReputation,
     writeMonthlyReputation)), "monthlyReputationActor")
   val quarterlyReputationActorRef = system.actorOf(Props(new QuarterlyReputationActor(quarterlyReputation,
     writeQuarterlyReputation)), "quarterlyReputationActor")
-  val scriptActorRef = system.actorOf(Props(new ScriptActor(allTimeReputationActorRef, monthlyReputationActorRef,
-    quarterlyReputationActorRef, storeBlogs, blogs)), "ScriptActor")
-
+  val blogScriptActorRef = system.actorOf(Props(new BlogScriptActor(allTimeReputationActorRef,
+    monthlyReputationActorRef, quarterlyReputationActorRef, storeBlogs, blogs)), "BlogScriptActor")
+  val knolxScriptActorRef = system.actorOf(Props(new KnolxScriptActor(allTimeReputationActorRef,
+    monthlyReputationActorRef, quarterlyReputationActorRef, storeKnolx, knolx)), "KnolxScriptActor")
   val latestBlogs = blogs.getLatestBlogsFromAPI
   storeBlogs.insertBlog(latestBlogs)
+  val latestKnolx = knolx.getLatestKnolxFromAPI
+  storeKnolx.insertKnolx(latestKnolx)
   val allTimeReputations = allTimeReputation.getKnolderReputation
   writeAllTimeReputation.insertAllTimeReputationData(allTimeReputations)
   writeAllTimeReputation.updateAllTimeReputationData(allTimeReputations)
@@ -60,10 +64,9 @@ object DriverApp extends App {
   writeQuarterlyReputation.insertQuarterlyReputationData(quarterlyReputations)
   writeQuarterlyReputation.updateQuarterlyReputationData(quarterlyReputations)
   reputationOnAPI.displayReputationOnAPI
-
   val indiaCurrentTime = Constant.CURRENT_TIME
   val totalSecondsOfDayTillCurrentTime = indiaCurrentTime.toLocalTime.toSecondOfDay
-  val startTimeToScriptExecution = LocalTime.of(1, 0, 0, 0).toSecondOfDay
+  val startTimeToScriptExecution = LocalTime.of(0, 0, 0, 0).toSecondOfDay
   val secondsInDay = 24 * 60 * 60
   val timeForBlogScriptExecution =
     if (startTimeToScriptExecution - totalSecondsOfDayTillCurrentTime < 0) {
@@ -71,10 +74,16 @@ object DriverApp extends App {
     } else {
       startTimeToScriptExecution - totalSecondsOfDayTillCurrentTime
     }
-
   /**
    * Fetching latest blogs from Wordpress API and storing in database.
    */
-  system.scheduler.scheduleAtFixedRate(timeForBlogScriptExecution.seconds, 24.hours, scriptActorRef,
+  system.scheduler.scheduleAtFixedRate(timeForBlogScriptExecution.seconds, 24.hours, blogScriptActorRef,
     ExecuteBlogsScript)
+  /**
+   * Fetching latest knolx from knolx API and storing in database.
+   */
+  QuartzSchedulerExtension.get(system).createSchedule("knolxScriptScheduler", None,
+    "0 0 0 ? * 6 *", None, Constant.INDIAN_TIMEZONE)
+  QuartzSchedulerExtension.get(system).schedule("knolxScriptScheduler", knolxScriptActorRef,
+    ExecuteKnolxScript)
 }
