46624c3db75fc528951cdeebf9ea3ebbc7e7a311
mansi7babbar <mansi7babbar@gmail.com>
2020-06-25 13:06:57 +0530

    updated Downs part in database script

--- app/database_scripts/18-Jun-2020.sql
+++ app/database_scripts/18-Jun-2020.sql
@@ -9,5 +9,13 @@ ALTER TABLE quarterly_reputation DROP COLUMN IF EXISTS full_name CASCADE;
 -- !Downs
 
 ALTER TABLE all_time_reputation ADD COLUMN full_name VARCHAR(100);
+UPDATE all_time_reputation AS all_time SET full_name = (SELECT full_name FROM knolder WHERE all_time.knolder_id = knolder.id);
+ALTER TABLE all_time_reputation ALTER COLUMN full_name SET NOT NULL;
+
 ALTER TABLE monthly_reputation ADD COLUMN full_name VARCHAR(100);
+UPDATE monthly_reputation AS monthly SET full_name = (SELECT full_name FROM knolder WHERE monthly.knolder_id = knolder.id);
+ALTER TABLE monthly_reputation ALTER COLUMN full_name SET NOT NULL;
+
 ALTER TABLE quarterly_reputation ADD COLUMN full_name VARCHAR(100);
+UPDATE quarterly_reputation AS quarterly SET full_name = (SELECT full_name FROM knolder WHERE quarterly.knolder_id = knolder.id);
+ALTER TABLE quarterly_reputation ALTER COLUMN full_name SET NOT NULL;
