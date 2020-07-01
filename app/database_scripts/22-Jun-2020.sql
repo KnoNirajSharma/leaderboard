3e08abbbafa469c0f75fed80089bda6c3e0e74b8
abhinavsharma402 <abhinavsharma402@gmail.com>
2020-07-01 14:49:29 +0530

    update dml

--- app/database_scripts/22-Jun-2020.sql
+++ app/database_scripts/22-Jun-2020.sql
@@ -2,12 +2,10 @@
 
 -- !Ups
 
-<<<<<<< HEAD
 CREATE TABLE knolx(id VARCHAR(100) PRIMARY KEY NOT NULL, email_id VARCHAR(100) NOT NULL, delivered_on TIMESTAMP NOT NULL, title VARCHAR(255) NOT NULL);
-=======
-CREATE TABLE knolx(id INT PRIMARY KEY NOT NULL, email_id VARCHAR(100) NOT NULL, delivered_on TIMESTAMP NOT NULL, title VARCHAR(255) NOT NULL);
->>>>>>> 214137affeb3d92feca09a0b7a2211dc3995ad4b
 
 -- !Downs
 
 DROP TABLE knolx;
+
+
