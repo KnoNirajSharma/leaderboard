85e231fb6d9624c6960712a8c53f62aa417eb63f
abhinavsharma402 <abhinavsharma402@gmail.com>
2020-07-01 13:37:08 +0530

    update dml scripts

---
+++ app/database_scripts/22-Jun-2020.sql
@@ -0,0 +1,9 @@
+-- knoldus_leaderboard schema
+
+-- !Ups
+
+CREATE TABLE knolx(id VARCHAR(100) PRIMARY KEY NOT NULL, email_id VARCHAR(100) NOT NULL, delivered_on TIMESTAMP NOT NULL, title VARCHAR(255) NOT NULL);
+
+-- !Downs
+
+DROP TABLE knolx;
