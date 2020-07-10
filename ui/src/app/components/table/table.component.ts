8b3aa122f7efea8128eafa03c5952ab6359044a8
Sanjoli Varshney <varshney.sanjoli@gmail.com>
2020-07-10 14:43:26 +0530

    wrapped ion-card in ion-list, used bootstrap classes and rem is used

--- ui/src/app/components/table/table.component.ts
+++ ui/src/app/components/table/table.component.ts
@@ -18,19 +18,39 @@ export class TableComponent implements OnInit {
             name: 'Name',
             prop: 'knolderName',
             sortable: false,
-            headerClass: 'table-header-style',
-            cellClass: 'table-cell-style'
+            headerClass: 'text-body font-weight-bold h6',
+            cellClass: 'text-body p py-2'
         },
-        {name: 'Monthly Rank', prop: 'monthlyRank', headerClass: 'table-header-style', cellClass: 'table-cell-style'},
-        {name: 'Monthly Score', prop: 'monthlyScore', headerClass: 'table-header-style', cellClass: 'table-cell-style'},
-        {name: 'Overall Rank', prop: 'allTimeRank', headerClass: 'table-header-style', cellClass: 'table-cell-style'},
-        {name: 'Overall Score', prop: 'allTimeScore', headerClass: 'table-header-style', cellClass: 'table-cell-style'},
+        {
+            name: 'Monthly Rank',
+            prop: 'monthlyRank',
+            headerClass: 'text-body font-weight-bold h6',
+            cellClass: 'text-body p py-2'
+        },
+        {
+            name: 'Monthly Score',
+            prop: 'monthlyScore',
+            headerClass: 'text-body font-weight-bold h6',
+            cellClass: 'text-body p py-2'
+        },
+        {
+            name: 'Overall Rank',
+            prop: 'allTimeRank',
+            headerClass: 'text-body font-weight-bold h6',
+            cellClass: 'text-body p py-2'
+        },
+        {
+            name: 'Overall Score',
+            prop: 'allTimeScore',
+            headerClass: 'text-body font-weight-bold h6',
+            cellClass: 'text-body p py-2'
+        },
         {
             name: '3 Month Streak',
             prop: 'quarterlyStreak',
             sortable: false,
-            headerClass: 'table-header-style',
-            cellClass: 'table-cell-style'
+            headerClass: 'text-body font-weight-bold h6',
+            cellClass: 'text-body p py-2'
         }];
 
     constructor(public router: Router) {
