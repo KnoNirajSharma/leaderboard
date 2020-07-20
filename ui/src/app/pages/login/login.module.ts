ac09b3dde32ceb6387acfc4964c87c91280f38c2
umang-goyal <goel97umang@gmail.com>
2020-07-20 15:59:37 +0530

    formatted imports properly

--- ui/src/app/pages/login/login.module.ts
+++ ui/src/app/pages/login/login.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
 import { LoginPage } from './login.page';
-import {RouterModule, Routes} from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
