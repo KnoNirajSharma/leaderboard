c81efffa51a5287c54946cb0f2622f011186b00d
umang-goyal <goel97umang@gmail.com>
2020-08-26 15:41:16 +0530

    removed eventMock variables

--- ui/src/app/pages/main/main.page.spec.ts
+++ ui/src/app/pages/main/main.page.spec.ts
@@ -8,7 +8,6 @@ import { of } from 'rxjs';
 import { TableComponent } from '../../components/table/table.component';
 import { EmployeeFilterPipe } from '../../pipe/employee-filter.pipe';
 import { ReactiveFormsModule } from '@angular/forms';
-import { By } from '@angular/platform-browser';
 import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 import { ComponentsModule } from '../../components/components.module';
 import { ReputationModel } from '../../models/reputation.model';
@@ -132,47 +131,41 @@ describe('MainPage', () => {
 
   it('should sort list in asc on the basis of quarterly score', () => {
     component.filteredEmpData = [...dummyReputationData.reputation];
-    const eventMock = {newValue: 'asc', column: {prop: 'quarterlyStreak'}};
     spyOn(component, 'compareQuarterlyScore').and.returnValue(true);
-    component.sortTable(eventMock);
+    component.sortTable({newValue: 'asc', column: {prop: 'quarterlyStreak'}});
     expect(component.filteredEmpData[0].knolderId).toEqual(1);
   });
 
   it('should sort list in desc on the basis of quarterly score', () => {
     component.filteredEmpData = [...dummyReputationData.reputation];
-    const eventMock = {newValue: 'asc', column: {prop: 'quarterlyStreak'}};
     spyOn(component, 'compareQuarterlyScore').and.returnValue(false);
-    component.sortTable(eventMock);
+    component.sortTable({newValue: 'asc', column: {prop: 'quarterlyStreak'}});
     expect(component.filteredEmpData[0].knolderId).toEqual(2);
   });
 
   it('should sort the list in descending order on the basis of allTimeRank', () => {
     component.filteredEmpData = [...dummyReputationData.reputation];
-    const eventMock = {newValue: 'desc', column: {prop: 'allTimeRank'}};
-    component.sortTable(eventMock);
+    component.sortTable({newValue: 'desc', column: {prop: 'allTimeRank'}});
     expect(component.filteredEmpData[0].knolderId).toEqual(1);
   });
 
   it('should sort the list in descending order on the basis of allTimeScore', () => {
     component.filteredEmpData = [...dummyReputationData.reputation];
-    const eventMock = {newValue: 'desc', column: {prop: 'allTimeScore'}};
-    component.sortTable(eventMock);
+    component.sortTable({newValue: 'desc', column: {prop: 'allTimeScore'}});
     expect(component.filteredEmpData[0].knolderId).toEqual(2);
   });
 
   it('should sort the list in ascending order on the basis of monthlyScore', () => {
     component.filteredEmpData = [...dummyReputationData.reputation];
-    const eventMock = {newValue: 'asc', column: {prop: 'monthlyScore'}};
     spyOn(component, 'comparisonBasedOnAllTimeScore').and.returnValue(false);
-    component.sortTable(eventMock);
+    component.sortTable({newValue: 'asc', column: {prop: 'monthlyScore'}});
     expect(component.filteredEmpData[0].knolderId).toEqual(2);
   });
 
   it('should sort the list in ascending order on the basis of allTimeScore', () => {
     component.filteredEmpData = [...dummyReputationData.reputation];
-    const eventMock = {newValue: 'asc', column: {prop: 'allTimeScore'}};
     spyOn(component, 'comparisonBasedOnAllTimeScore').and.returnValue(true);
-    component.sortTable(eventMock);
+    component.sortTable({newValue: 'asc', column: {prop: 'allTimeScore'}});
     expect(component.filteredEmpData[0].knolderId).toEqual(1);
   });
 });
