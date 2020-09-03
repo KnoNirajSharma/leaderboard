0ccb5025865ea518dbd33794a6132271bafce9ec
Abhinav sharma <abhinavsharma402@gmail.com>
2020-09-03 19:36:34 +0530

    update changes in the code

--- app/database_scripts/31-aug-2020.sql
+++ app/database_scripts/31-aug-2020.sql
@@ -1,2 +1,6 @@
-CREATE TABLE oscontribution(id VARCHAR(255) PRIMARY KEY, email_id VARCHAR(100) NOT NULL, contributed_on TIMESTAMP NOT NULL, title VARCHAR(255) NOT NULL);
-CREATE TABLE conference(id VARCHAR(255) PRIMARY KEY, email_id VARCHAR(100) NOT NULL, delivered_on TIMESTAMP NOT NULL, title VARCHAR(255) NOT NULL);
+-- !Ups
+CREATE TABLE oscontribution(id VARCHAR(100) PRIMARY KEY, email_id VARCHAR(100) NOT NULL, contributed_on TIMESTAMP NOT NULL, title VARCHAR(255) NOT NULL);
+CREATE TABLE conference(id VARCHAR(100) PRIMARY KEY, email_id VARCHAR(100) NOT NULL, delivered_on TIMESTAMP NOT NULL, title VARCHAR(255) NOT NULL);
+-- !Downs
+DROP TABLE oscontribution;
+DROP TABLE conference;
