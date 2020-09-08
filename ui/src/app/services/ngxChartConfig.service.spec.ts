3cdbf89ae8795f942dc13aa4dba70d249e256939
umang-goyal <goel97umang@gmail.com>
2020-09-08 17:57:33 +0530

    refactored spec file of ngxChartConfig

--- ui/src/app/services/ngxChartConfig.service.spec.ts
+++ ui/src/app/services/ngxChartConfig.service.spec.ts
@@ -3,30 +3,33 @@ import { NgxChartConfigService } from './ngxChartConfig.service';
 
 fdescribe('ngxChartConfiguration', () => {
     let ngxChartConfigService: NgxChartConfigService;
+    const mockVerticalBarConfig = {
+        barPadding: 1,
+        yAxisLabel: 'test'
+    };
+    const mockColorScheme = {
+        domain: ['test']
+    };
 
     beforeEach(() => {
         TestBed.configureTestingModule({
             providers: [NgxChartConfigService]
         });
         ngxChartConfigService = TestBed.get(NgxChartConfigService);
-        ngxChartConfigService.verticalBarChartConfigs = {
-            barPadding: 1,
-            yAxisLabel: 'test'
-        };
-        ngxChartConfigService.chartColorScheme = {
-            domain: ['test']
-        };
     });
 
     it('should get barPadding', () => {
+        ngxChartConfigService.verticalBarChartConfigs = mockVerticalBarConfig;
         expect(ngxChartConfigService.verticalBarChartPadding).toEqual(1);
     });
 
     it('should get barPadding', () => {
+        ngxChartConfigService.verticalBarChartConfigs = mockVerticalBarConfig;
         expect(ngxChartConfigService.verticalBarChartYLabel).toEqual('test');
     });
 
     it('should get barPadding', () => {
+        ngxChartConfigService.chartColorScheme = mockColorScheme;
         expect(ngxChartConfigService.colorScheme).toEqual({
             domain: ['test']
         });
