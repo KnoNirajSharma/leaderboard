d9d3ca38bdd423d165ca987c0d3bc78f26afdbb1
Abhinav sharma <abhinavsharma402@gmail.com>
2020-09-08 16:56:31 +0530

    update changes in code

--- app/src/main/scala/com/knoldus/leader_board/business/WebinarSpreadSheetDataImpl.scala
+++ app/src/main/scala/com/knoldus/leader_board/business/WebinarSpreadSheetDataImpl.scala
@@ -1,7 +1,7 @@
 package com.knoldus.leader_board.business
 
 import com.knoldus.leader_board.Webinar
-import com.knoldus.leader_board.utils.{SpreadSheetApi, SpreadSheetApiImpl}
+import com.knoldus.leader_board.utils.SpreadSheetApi
 import com.typesafe.config.Config
 import com.typesafe.scalalogging.LazyLogging
 
