e5d67c92aae0fc6ee0d42b5b3c3b5c807883d3d0
umang-goyal <goel97umang@gmail.com>
2020-09-14 14:18:42 +0530

    enabled back button in header

--- ui/src/app/pages/hall-of-fame/hall-of-fame.page.ts
+++ ui/src/app/pages/hall-of-fame/hall-of-fame.page.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hall-of-fame.page.scss'],
 })
 export class HallOfFamePage implements OnInit {
+  mainPageLink = '/';
   list = [
     { month: 'August',
       year: 2020,
