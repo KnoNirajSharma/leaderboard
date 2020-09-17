a5eaed7acbfef1750b13e9898d9d0b144a6fb999
umang-goyal <goel97umang@gmail.com>
2020-09-17 22:02:55 +0530

    reduced html code by using Object keys

--- ui/src/app/components/medal-tally/medal-tally.component.ts
+++ ui/src/app/components/medal-tally/medal-tally.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { MedalTallyModel } from '../../models/medalTally.model';
 
 @Component({
@@ -6,6 +6,16 @@ import { MedalTallyModel } from '../../models/medalTally.model';
   templateUrl: './medal-tally.component.html',
   styleUrls: ['./medal-tally.component.scss'],
 })
-export class MedalTallyComponent {
+export class MedalTallyComponent implements OnInit {
   @Input() medalTally: MedalTallyModel;
+  medalTallyKeys: string[];
+  imageSrcMap = {
+    gold: './assets/icon/gold-medal.svg',
+    silver: './assets/icon/silver-medal.svg',
+    bronze: './assets/icon/bronze-medal.svg'
+  };
+
+  ngOnInit() {
+    this.medalTallyKeys = Object.keys(this.medalTally);
+  }
 }
