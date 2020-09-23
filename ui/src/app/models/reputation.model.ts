2a1d080e52d5e112f3ac9e998299d92c8a10d512
umang-goyal <goel97umang@gmail.com>
2020-09-23 16:18:08 +0530

    removed mock url in employee activity service

--- ui/src/app/models/reputation.model.ts
+++ ui/src/app/models/reputation.model.ts
@@ -4,8 +4,8 @@ export interface ReputationModel {
   blogs: { monthly: number; allTime: number; };
   knolx: { monthly: number; allTime: number; };
   webinars: { monthly: number; allTime: number; };
-  techhubTemplate: { monthly: number; allTime: number; };
-  osContribution: { monthly: number; allTime: number; };
+  techhubTemplates: { monthly: number; allTime: number; };
+  osContributions: { monthly: number; allTime: number; };
   conferences: { monthly: number; allTime: number; };
   books: { monthly: number; allTime: number; };
   researchPapers: { monthly: number; allTime: number; };
