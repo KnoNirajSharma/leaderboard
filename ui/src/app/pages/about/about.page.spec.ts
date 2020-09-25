e523660c80ed9a656eb7df51e7b274e0fd604d01
umang-goyal <goel97umang@gmail.com>
2020-09-25 17:27:41 +0530

    updated test cases for about page

--- ui/src/app/pages/about/about.page.spec.ts
+++ ui/src/app/pages/about/about.page.spec.ts
@@ -1,7 +1,15 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
-
 import { AboutPage } from './about.page';
+import { ComponentsModule } from '../../components/components.module';
+import {RouterTestingModule} from '@angular/router/testing';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {BsDatepickerModule} from 'ngx-bootstrap/datepicker';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {AngularFireModule} from '@angular/fire';
+import {environment} from '../../../environments/environment';
+import {AngularFirestoreModule} from '@angular/fire/firestore';
+import {AngularFireAuthModule} from '@angular/fire/auth';
 
 describe('AboutPage', () => {
   let component: AboutPage;
@@ -10,7 +18,15 @@ describe('AboutPage', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ AboutPage ],
-      imports: [IonicModule.forRoot()]
+      imports: [
+        IonicModule.forRoot(),
+        RouterTestingModule,
+        ComponentsModule,
+        RouterTestingModule,
+        AngularFireModule.initializeApp(environment.firebaseConfig, 'angular-auth-firebase'),
+        AngularFirestoreModule,
+        AngularFireAuthModule
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(AboutPage);
