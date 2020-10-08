d6b07d2d0376b9e4b73929e1d78cd902b0338f18
Abhinav sharma <abhinavsharma402@gmail.com>
2020-10-08 12:02:30 +0530

    update ddl file

--- app/database_scripts/5-october-2020.sql
+++ app/database_scripts/5-october-2020.sql
@@ -1,5 +1,5 @@
 -- !Ups
-CREATE TABLE  dynamicscoring(id Serial PRIMARY KEY,blog_score_multiplier INT NOT NULL,
+CREATE TABLE IF NOT EXISTS dynamicscoring(id Serial PRIMARY KEY,blog_score_multiplier INT NOT NULL,
 knolx_score_multiplier INT NOT NULL,
 webinar_score_multiplier INT NOT NULL,
 oscontribution_score_multiplier INT NOT NULL,
@@ -24,5 +24,5 @@ month VARCHAR(255),
 year INT NOT NULL
 );
 -- !Downs
-DROP TABLE dynamicscoring;
-DROP TABLE monthlycontribution;
+DROP TABLE dynamicscoring IF EXISTS;
+DROP TABLE monthlycontribution IF EXISTS;
