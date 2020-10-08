f1a49a8a2157acf1c413361aea2c81b02b7e0f03
umang-goyal <goel97umang@gmail.com>
2020-10-08 17:48:26 +0530

    corrected api endpoints

--- ui/src/environments/environment.ts
+++ ui/src/environments/environment.ts
@@ -5,7 +5,7 @@ export const environment = {
     version: '1.0.0',
     baseUrl: 'http://35.232.185.162:8000/',
     routes: {
-      author: { endpoint: 'reputatio', method: 'GET' },
+      author: { endpoint: 'reputation', method: 'GET' },
       trends: { endpoint: 'reputation/twelvemonths', method: 'GET' },
       hallOfFame: { endpoint: 'reputation/halloffame', method: 'GET' }
     }
