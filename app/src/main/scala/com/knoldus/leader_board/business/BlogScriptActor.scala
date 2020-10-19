bd33386c77724e6e8b0dc4895d7dda83c4c7a643
Abhinav sharma <abhinavsharma402@gmail.com>
2020-10-19 10:06:12 +0530

    update logggers

--- app/src/main/scala/com/knoldus/leader_board/business/BlogScriptActor.scala
+++ app/src/main/scala/com/knoldus/leader_board/business/BlogScriptActor.scala
@@ -22,14 +22,14 @@ class BlogScriptActor(allTimeReputationActorRef: ActorRef, monthlyReputationActo
     case UpdateAndInsertMonthlyContribution => val firstDayOfCurrentMonth = IndianTime.currentTime.withDayOfMonth(1).toLocalDate
       val currentDayOfCurrentMonth = IndianTime.currentTime.toLocalDate
       if (firstDayOfCurrentMonth == currentDayOfCurrentMonth) {
-        logger.info("fetching and updating last month score")
+        logger.info("fetching and updating last month score if the 1st day of month after fetching blog details")
         val month = IndianTime.currentTime.minusMonths(1).getMonth.toString
         val year = IndianTime.currentTime.minusMonths(1).getYear
         val knolderMonthlyContributionScore = knolderMonthlyContribution.getKnolderMonthlyContribution(month, year)
         writeMonthlyContribution.insertKnolderMonthlyContribution(knolderMonthlyContributionScore)
         writeMonthlyContribution.updateKnolderMonthlyContribution(knolderMonthlyContributionScore)
       } else {
-        logger.info("fetching and updating current month score")
+        logger.info("fetching and updating current month score if there is no 1st day of month after fetching blog details")
         val month = IndianTime.currentTime.getMonth.toString
         val year = IndianTime.currentTime.getYear
         val knolderMonthlyContributionScore = knolderMonthlyContribution.getKnolderMonthlyContribution(month, year)
--- app/src/main/scala/com/knoldus/leader_board/business/WebinarScriptActor.scala
+++ app/src/main/scala/com/knoldus/leader_board/business/WebinarScriptActor.scala
@@ -21,14 +21,14 @@ class WebinarScriptActor(allTimeReputationActorRef: ActorRef, monthlyReputationA
     case UpdateAndInsertMonthlyContribution => val firstDayOfCurrentMonth = IndianTime.currentTime.withDayOfMonth(1).toLocalDate
       val currentDayOfCurrentMonth = IndianTime.currentTime.toLocalDate
       if (firstDayOfCurrentMonth == currentDayOfCurrentMonth) {
-        logger.info("fetching and updating last month score")
+        logger.info("fetching and updating last month score if the 1st day of month after fetching webinar details")
         val month = IndianTime.currentTime.minusMonths(1).getMonth.toString
         val year = IndianTime.currentTime.minusMonths(1).getYear
         val knolderMonthlyContributionScore = knolderMonthlyContribution.getKnolderMonthlyContribution(month, year)
         writeMonthlyContribution.insertKnolderMonthlyContribution(knolderMonthlyContributionScore)
         writeMonthlyContribution.updateKnolderMonthlyContribution(knolderMonthlyContributionScore)
       } else {
-        logger.info("fetching and updating current month score")
+        logger.info("fetching and updating current month score if there is no 1st day of month after fetching webinar details")
         val month = IndianTime.currentTime.getMonth.toString
         val year = IndianTime.currentTime.getYear
         val knolderMonthlyContributionScore = knolderMonthlyContribution.getKnolderMonthlyContribution(month, year)
