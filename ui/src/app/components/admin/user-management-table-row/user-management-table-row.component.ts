da67b03910938d640b7f6c48d1bc753ad03b47a3
umang-goyal <goel97umang@gmail.com>
2020-11-17 18:29:58 +0530

    doubts

--- ui/src/app/components/admin/user-management-table-row/user-management-table-row.component.ts
+++ ui/src/app/components/admin/user-management-table-row/user-management-table-row.component.ts
@@ -9,8 +9,19 @@ import {UserDetailsModel} from '../../../models/user-details.model';
 })
 export class UserManagementTableRowComponent implements OnInit {
     @Input() userDetails: UserDetailsModel;
+    actions = [];
 
     ngOnInit() {
+        this.setAction();
     }
 
+    setAction() {
+        if (!this.userDetails.accountActive) {
+            this.actions = ['Enable Account'];
+        } else if (this.userDetails.role === 'admin') {
+            this.actions = ['Disable Account', 'Remove Admin'];
+        } else {
+            this.actions = ['Disable Account', 'Make Admin'];
+        }
+    }
 }
