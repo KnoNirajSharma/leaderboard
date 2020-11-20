6de195b3abace8f645f7ab63530235c2936c0cb0
umang-goyal <goel97umang@gmail.com>
2020-11-20 13:48:09 +0530

    removed unused imports in dropdown menu list component

--- ui/src/app/components/common/dropdown-wrapper/dropdown-wrapper.component.spec.ts
+++ ui/src/app/components/common/dropdown-wrapper/dropdown-wrapper.component.spec.ts
@@ -1,46 +1,46 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { IonicModule } from '@ionic/angular';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {IonicModule} from '@ionic/angular';
 
-import { DropdownWrapperComponent } from './dropdown-wrapper.component';
+import {DropdownWrapperComponent} from './dropdown-wrapper.component';
 
 describe('DropdownWrapperComponent', () => {
-  let component: DropdownWrapperComponent;
-  let fixture: ComponentFixture<DropdownWrapperComponent>;
+    let component: DropdownWrapperComponent;
+    let fixture: ComponentFixture<DropdownWrapperComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ DropdownWrapperComponent ],
-      imports: [IonicModule.forRoot()],
-    }).compileComponents();
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [DropdownWrapperComponent],
+            imports: [IonicModule.forRoot()],
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(DropdownWrapperComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  }));
+        fixture = TestBed.createComponent(DropdownWrapperComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    }));
 
-  it('should make dropdownMenuVisibility to false if action is close', () => {
-    component.dropdownMenuVisibility = true;
-    component.controlDropdownMenu('close');
-    expect(component.dropdownMenuVisibility).toBeFalsy();
-  });
+    it('should make dropdownMenuVisibility to false if action is close', () => {
+        component.dropdownMenuVisibility = true;
+        component.controlDropdownMenu('close');
+        expect(component.dropdownMenuVisibility).toBeFalsy();
+    });
 
-  it('should toggle dropdownMenuVisibility if action is toggle', () => {
-    component.dropdownMenuVisibility = false;
-    component.controlDropdownMenu('toggle');
-    expect(component.dropdownMenuVisibility).toBeTruthy();
-  });
+    it('should toggle dropdownMenuVisibility if action is toggle', () => {
+        component.dropdownMenuVisibility = false;
+        component.controlDropdownMenu('toggle');
+        expect(component.dropdownMenuVisibility).toBeTruthy();
+    });
 
-  it('should call controlDropdownMenu this close parameter', () => {
-    spyOn(component, 'controlDropdownMenu');
-    spyOn(component.dropdownIconRef.nativeElement, 'contains').and.returnValue(false);
-    component.onClick('<div>outside div</div>');
-    expect(component.controlDropdownMenu).toHaveBeenCalledWith('close');
-  });
+    it('should call controlDropdownMenu this close parameter', () => {
+        spyOn(component, 'controlDropdownMenu');
+        spyOn(component.dropdownIconRef.nativeElement, 'contains').and.returnValue(false);
+        component.onClick('<div>outside div</div>');
+        expect(component.controlDropdownMenu).toHaveBeenCalledWith('close');
+    });
 
-  it('should call controlDropdownMenu this toggle parameter', () => {
-    spyOn(component, 'controlDropdownMenu');
-    spyOn(component.dropdownIconRef.nativeElement, 'contains').and.returnValue(true);
-    component.onClick('<div>inside elem</div>');
-    expect(component.controlDropdownMenu).toHaveBeenCalledWith('toggle');
-  });
+    it('should call controlDropdownMenu this toggle parameter', () => {
+        spyOn(component, 'controlDropdownMenu');
+        spyOn(component.dropdownIconRef.nativeElement, 'contains').and.returnValue(true);
+        component.onClick('<div>inside elem</div>');
+        expect(component.controlDropdownMenu).toHaveBeenCalledWith('toggle');
+    });
 });
