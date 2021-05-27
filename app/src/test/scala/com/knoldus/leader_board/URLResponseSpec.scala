adfec2ce9529bf0c48d70c1d11637fec279c1e2e
Sonu <sonu.mehrotra@knoldus.com>
2021-05-27 19:15:49 +0800

    KL-380: If applied, this commit fixes a typo in the test file

--- app/src/test/scala/com/knoldus/leader_board/URLResponseSpec.scala
+++ app/src/test/scala/com/knoldus/leader_board/URLResponseSpec.scala
@@ -1,23 +1,18 @@
 package com.knoldus.leader_board
 
 import com.knoldus.leader_board.business.URLResponse
-import org.apache.http.HttpRequest
 import org.apache.http.client.methods.HttpGet
-import org.mockito.MockitoSugar
-import org.scalatest.BeforeAndAfterEach
 import org.scalatest.wordspec.AnyWordSpecLike
 
-class URLResponseSpec extends AnyWordSpecLike with MockitoSugar with BeforeAndAfterEach  {
-  val response=new URLResponse
+class URLResponseSpec extends AnyWordSpecLike {
+  val response = new URLResponse
 
   "URl response" should {
-    "return empty json when url thrwon any exception" in{
+    "return an empty json response when url throws an exception" in {
+      val url         = "bloog.knoldus.com"
+      val httpRequest = new HttpGet(url)
 
-      val url="bloog.knoldus.com"
-      val httpRequest=new HttpGet(url)
-     assert( response.getResponse(httpRequest)== """[]""")
+      assert(response.getResponse(httpRequest) == """[]""")
     }
-
   }
-
 }
